18a14c521cb5307391bfcab923565b7e0b95d2be
Artem <artyom.zavyalov2005@yandex.ru>
2026-05-09 22:09:43 +0500

    готово

--- config/config.php
+++ config/config.php
@@ -1,9 +1,10 @@
 <?php
+
 define('BASE_PATH', realpath(__DIR__ . '/..'));
 define('TEMPLATES_DIR', BASE_PATH . '/templates/');
 define('LAYOUTS_DIR', 'layouts/');
 
-include __DIR__ . "/../engine/bux.php";
-include __DIR__ . "/../engine/functions.php";
-include __DIR__ . "/../engine/catalog.php";
-include __DIR__ . "/../engine/log.php";
+include BASE_PATH . '/engine/bux.php';
+include BASE_PATH . '/engine/functions.php';
+include BASE_PATH . '/engine/catalog.php';
+include BASE_PATH . '/engine/log.php';
